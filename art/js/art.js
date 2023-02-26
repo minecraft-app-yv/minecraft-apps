8cf0d1eedcdaae406aa4d893f03e47aa83394c47
minecraft-app-yv <126371351+minecraft-app-yv@users.noreply.github.com>
2023-02-26 14:04:09 +0900

    Update art.js

--- art/js/art.js
+++ art/js/art.js
@@ -4998,7 +4998,8 @@ function delete_cookie (dt,path) {
 /*https://javascript.programmer-reference.com/js-onunload/*/
 /**/
 window.onload = function(){
-    console.log('t');
+    console.log('m');
+  console.log(document.cookie);
   // keyを指定して取得
   //「 key1=val1; key2=val2; key3=val3; ・・・ 」というCookie情報が保存されているとする
   let arr = getCookieArray();
