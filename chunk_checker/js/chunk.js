2ef348e0d5621302ab3a020e5b56c7363417a0b4
Yusuke M <126371351+minecraft-app-yv@users.noreply.github.com>
2023-08-05 15:18:27 +0900

    Add files via upload

--- chunk_checker/js/chunk.js
+++ chunk_checker/js/chunk.js
@@ -1,4 +1,5 @@
 /*++reserve functions++*/
+let obj = {td_x: '', tr_z: '', view: '', start_x: '', start_y: ''};
 /*aside display at 1200px*/
 $(window).resize(function() {
   let ww = window.innerWidth;
@@ -42,42 +43,37 @@ $(document).ready(function () {
     $('#hanb').prop('checked', false);
   }
 });
+/*++main++*/
 /*Map Calculation*/
-$('.go_button').on("click", function (e) {
+function map_calcu () {
   $("#answer").removeClass("hidden");
   let wh = window.innerHeight;
   wh = wh - 80;
   $('table').css('max-height', wh);
-  if (!$('input[name="x"]').val()) {
-    $('input[name="x"]').val(123);
-  }
-  if (!$('input[name="z"]').val()) {
-    $('input[name="z"]').val(123);
-  }
   let x = $('input[name="x"]').val();
   let z = $('input[name="z"]').val();
   let chunkCount = $('select[name="count"]').val();
-  let view = $('input[name="view"]:checked').val();
+  obj.view = $('input[name="view"]:checked').val();
   let area = chunkCount * 2 + 1;
   let ansx = Math.floor(x / 16) * 16 - 16 * chunkCount;
   let ansz = Math.floor(z / 16) * 16 - 16 * chunkCount;
   /*simple display*/
-  if (view == 1) {
+  if (obj.view == 1) {
     let i = 0;
     let col = "";
     let colEnpty = "";
     let countX = 0;
     while (i <= 16 * area) {
       if (ansx % 16 === 0) {
-        col = col + '<td style="padding: 0.5em;" class="x16 tableTop positionX' + ansx + '">' + ansx + "</td>";
-        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x16 positionX' + ansx + '"><span class="hide countX' + countX + '">x' + ansx + "<br></span></td>";
+        col = col + '<th style="padding: 0.5em;" class="x16 tableTop" data-p="x' + ansx + '">' + ansx + "</th>";
+        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x16" data-p="x' + ansx + '"><span class="hide countX' + countX + '">x' + ansx + "<br></span></td>";
         countX = countX + 1;
       } else if (((ansx % 16) + 1) % 16 === 0) {
-        col = col + '<td style="padding: 0.5em;" class="x15 tableTop positionX' + ansx + '">' + ansx + "</td>";
-        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x15 positionX' + ansx + '"><span class="hide countXbef' + countX + '">x' + ansx + "<br></span></td>";
+        col = col + '<th style="padding: 0.5em;" class="x15 tableTop" data-p="x' + ansx + '">' + ansx + "</th>";
+        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x15" data-p="x' + ansx + '"><span class="hide countXbef' + countX + '">x' + ansx + "<br></span></td>";
       } else if (((ansx % 16) - 1) % 16 === 0) {
-        col = col + '<td class="tableTopNone positionX' + ansx + '">X</td>';
-        colEnpty = colEnpty + '<td class="positionX' + ansx + '"></td>';
+        col = col + '<th class="tableTopNone" data-p="x' + ansx + '">X</th>';
+        colEnpty = colEnpty + '<td data-p="x' + ansx + '"></td>';
       }
       ansx = ansx + 1;
       i = i + 1;
@@ -85,22 +81,23 @@ $('.go_button').on("click", function (e) {
 
     let j = 0;
     let positionZ = "";
-    let table = '<tr><td class = "fixed"></td>' + col + "</tr>";
+    let table = '<thead><tr><th class = "fixed"></th>' + col + "</tr></thead><tbody>";
     let countZ = 0;
     while (j <= 16 * area) {
       if (ansz % 16 === 0) {
-        table = table + '<tr class="z16 positionZ' + ansz + " countZ" + countZ + '"><td style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
-          ansz + "</span>" + ansz + "</td>" + colEnpty + "</tr>";
+        table = table + '<tr class="z16 countZ' + countZ + '" data-p="z' + ansz + '"><th style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
+          ansz + "</span>" + ansz + "</th>" + colEnpty + "</tr>";
         countZ = countZ + 1;
       } else if (((ansz % 16) + 1) % 16 === 0) {
-        table = table + '<tr class="z15 positionZ' + ansz + " countZbef" + countZ + '"><td style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
-          ansz + "</span>" + ansz + "</td>" + colEnpty + "</tr>";
+        table = table + '<tr class="z15 countZbef' + countZ + '" data-p="z' + ansz + '"><th style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
+          ansz + "</span>" + ansz + "</th>" + colEnpty + "</tr>";
       } else if (((ansz % 16) - 1) % 16 === 0) {
-        table = table + '<tr class="positionZ' + ansz + '"><td class="tableLeftNone">X</td>' + colEnpty + "</tr>";
+        table = table + '<tr data-p="z' + ansz + '"><th class="tableLeftNone">X</th>' + colEnpty + "</tr>";
       }
       ansz = ansz + 1;
       j = j + 1;
     }
+    table += '</tbody>';
     $("#answer").children().remove();
     $("#answer").append(table);
 
@@ -116,55 +113,57 @@ $('.go_button').on("click", function (e) {
     } else {
       myZ = Math.floor(z / 16) * 16 + 1;
     }
-
-    $("#answer td").append('<p class="space"></p>');
-    $("#answer tr.positionZ" + myZ + " td.positionX" + myX + " p.space").attr("id", "here");
+    obj.td_x = myX;
+    obj.tr_z = myZ;
+    $('#answer tr[data-p="z' + myZ + '"] td[data-p="x' + myX + '"]').append('<p class="space"></p>');
+    $('#answer tr[data-p="z' + myZ + '"] td[data-p="x' + myX + '"] p.space').attr("id", "here");
   }
   /*detailed display*/
-  if (view == 2) {
+  if (obj.view == 2) {
     let i = 0;
     let col = "";
     let colEnpty = "";
     let countX = 0;
     while (i <= 16 * area) {
       if (ansx % 16 === 0) {
-        col = col + '<td style="padding: 0.5em;" class="x16 tableTop positionX' + ansx + '">' + ansx + "</td>";
-        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x16 positionX' + ansx + '"><span class="hide countX' + countX + '">x' + ansx + "<br></span></td>";
+        col = col + '<th style="padding: 0.5em;" class="x16 tableTop" data-p="x' + ansx + '">' + ansx + "</th>";
+        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x16" data-p="x' + ansx + '"><span class="hide countX' + countX + '">x' + ansx + "<br></span></td>";
         countX = countX + 1;
       } else if (((ansx % 16) + 1) % 16 === 0) {
-        col = col + '<td style="padding: 0.5em;" class="x15 tableTop positionX' + ansx + '">' + ansx + "</td>";
-        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x15 positionX' + ansx + '"><span class="hide countXbef' + countX + '">x' + ansx + "<br></span></td>";
+        col = col + '<th style="padding: 0.5em;" class="x15 tableTop" data-p="x' + ansx + '">' + ansx + "</th>";
+        colEnpty = colEnpty + '<td style="padding: 0.5em;" class="x15" data-p="x' + ansx + '"><span class="hide countXbef' + countX + '">x' + ansx + "<br></span></td>";
       } else {
-        col = col + '<td style="padding: 0.5em;" class="tableTop positionX' + ansx + '">' + ansx + "</td>";
-        colEnpty = colEnpty + '<td class="positionX' + ansx + '"></td>';
+        col = col + '<th style="padding: 0.5em;" class="tableTop" data-p="x' + ansx + '">' + ansx + "</th>";
+        colEnpty = colEnpty + '<td data-p="x' + ansx + '"></td>';
       }
       ansx = ansx + 1;
       i = i + 1;
     }
 
     let j = 0;
-    let table = '<tr><td class = "fixed"></td>' + col + "</tr>";
+    let table = '<thead><tr><th class = "fixed"></th>' + col + '</tr></thead><tbody>';
     let countZ = 0;
     while (j <= 16 * area) {
       if (ansz % 16 === 0) {
-        table = table + '<tr class="z16 positionZ' + ansz + " countZ" + countZ + '"><td style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
-          ansz + "</span>" + ansz + "</td>" + colEnpty + "</tr>";
+        table = table + '<tr class="z16 countZ' + countZ + '" data-p="z' + ansz + '"><th style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
+          ansz + "</span>" + ansz + "</th>" + colEnpty + "</tr>";
         countZ = countZ + 1;
       } else if (((ansz % 16) + 1) % 16 === 0) {
-        table = table + '<tr class="z15 positionZ' + ansz + " countZbef" + countZ + '"><td style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
-          ansz + "</span>" + ansz + "</td>" + colEnpty + "</tr>";
+        table = table + '<tr class="z15 countZbef' + countZ + '" data-p="z' + ansz + '"><th style="padding: 0.5em;" class="tableLeft"><span class="hide cZ">z' +
+          ansz + "</span>" + ansz + "</th>" + colEnpty + "</tr>";
       } else {
-        table = table + '<tr class="positionZ' + ansz + '"><td style="padding: 0.5em;" class="tableLeft">' + ansz + "</td>" + colEnpty + "</tr>";
+        table = table + '<tr data-p="z' + ansz + '"><th style="padding: 0.5em;" class="tableLeft">' + ansz + "</th>" + colEnpty + "</tr>";
       }
       ansz = ansz + 1;
       j = j + 1;
     }
-
+    table += '</tbody>';
     $("#answer").children().remove();
     $("#answer").append(table);
-
-    $("#answer td").append('<p class="space"></p>');
-    $("#answer tr.positionZ" + z + " td.positionX" + x + " p.space").attr( "id", "here");
+    obj.td_x = x;
+    obj.tr_z = z;
+    $('#answer tr[data-p="z' + z + '"] td[data-p="x' + x + '"]').append('<p class="space"></p>');
+    $('#answer tr[data-p="z' + z + '"] td[data-p="x' + x + '"] p.space').attr("id", "here");
   }
   /*show coordinates*/
   let coordinate = $('input[name="coordinate"]:checked').val();
@@ -179,18 +178,41 @@ $('.go_button').on("click", function (e) {
   let n = 0;
   while (n <= area) {
     for (let m = 0; m <= area; m = m + 1) {
-      let cou_Z = $("tr.countZ" + n + " span.cZ").text() + "<br>";
-      let cou_Z_bef = $("tr.countZbef" + n + " span.cZ").text() + "<br>";
+      let cou_Z = $("tr.countZ" + n + " span.cZ").text();
+      let cou_Z_bef = $("tr.countZbef" + n + " span.cZ").text();
       $("tr.countZ" + n + " span.countX" + m + "").append(cou_Z);
       $("tr.countZbef" + n + " span.countXbef" + m + "").append(cou_Z_bef);
     }
     n = n + 1;
   }
-  /*move to my place
-  https://code-pocket.info/20191109272/
-https://developer.mozilla.org/ja/docs/Web/API/Element/scrollIntoView*/
+  /*move to my place*/
+  /*https://code-pocket.info/20191109272/*/
+  /*https://developer.mozilla.org/ja/docs/Web/API/Element/scrollIntoView*/
   let target = document.getElementById("here");
   target.scrollIntoView({ block: "center", inline: "center" });
+  change_command ();
+}
+$('.go_button').on("click", function (e) {
+  if (!$('input[name="x"]').val()) {
+    $('input[name="x"]').val(123);
+  }
+  if (!$('input[name="z"]').val()) {
+    $('input[name="z"]').val(123);
+  }
+  try {
+    const inputText1 = $('input[name="x"]').val();
+    const inputText2 = $('input[name="z"]').val();
+    if (!/^(-?[0-9]+)$/.test(inputText1)) {
+      throw new Error('半角数字またはマイナス記号以外が入力されています。');
+    }
+    if (!/^(-?[0-9]+)$/.test(inputText2)) {
+      throw new Error('半角数字またはマイナス記号以外が入力されています。');
+    }
+    // ここに正常な処理を追加
+    map_calcu ();
+  } catch (error) {
+    alert(error.message);
+  }
 });
 /*move to #here at click my place icon*/
 $(".aside_menu .my_place").click(function () {
@@ -213,6 +235,107 @@ $('input[name="view"]').change(function () {
     $('select[name="count"] option[value="6"]').removeClass("hide");
   }
 });
+//++observer cp start++
+/*change command*/
+function write_command(str) {
+  $('#codeToCopy').text(str);
+  $('#copyButton').html('<i class="fa-solid fa-copy"></i>');
+}
+function get_xz() {
+  let td_x,tr_z;
+  let elem = document.elementFromPoint(obj.start_x, obj.start_y);
+  if (elem === null) {
+    return true;
+  }
+  let $element = jQuery(elem);
+  if ($element.get(0).tagName !== "TD" && $element.parent().get(0).tagName !== "TD") {
+    return false;
+  }
+  if ($element.get(0).tagName === "TD") {
+    td_x = $element.attr('data-p');
+    tr_z = $element.parent().attr('data-p');
+  }
+  if ($element.parent().get(0).tagName === "TD") {
+    td_x = $element.parent().attr('data-p');
+    tr_z = $element.parent().parent().attr('data-p');
+  }
+  td_x = td_x.substring(1);
+  tr_z = tr_z.substring(1);
+  obj.td_x = Number(td_x);
+  obj.tr_z = Number(tr_z);
+}
+function change_command () {
+  all_removeE ();
+
+  if (obj.view == 1) {
+    let x = Math.floor(obj.td_x / 16) * 16;
+    let z = Math.floor(obj.tr_z / 16) * 16;
+    let str = '/tp @p ' + x + ' ~ ' + z;
+    write_command(str);
+    $('#answer tbody td.command_target').removeClass('command_target');
+    $('#answer tbody tr[data-p="z' + z + '"] td[data-p="x' + x + '"]').addClass('command_target');
+  }
+  if (obj.view == 2) {
+    let str = '/tp @p ' + obj.td_x + ' ~ ' + obj.tr_z;
+    write_command(str);
+    $('#answer tbody td.command_target').removeClass('command_target');
+    $('#answer tbody tr[data-p="z' + obj.tr_z + '"] td[data-p="x' + obj.td_x + '"]').addClass('command_target');
+  }
+}
+function all_removeE () {
+  document.querySelector('#answer tbody').removeEventListener('mousemove', all_removeE);
+  document.querySelector('#answer tbody').removeEventListener('mouseup', change_command);
+  document.querySelector('#answer tbody').removeEventListener('touchmove', all_removeE);
+  document.querySelector('#answer tbody').removeEventListener('touchend', change_command);
+}
+function map_mousedown (e) {
+  obj.start_x = e.clientX;
+  obj.start_y = e.clientY;
+  get_xz();
+  document.querySelector('#answer tbody').addEventListener('mousemove', all_removeE);
+  document.querySelector('#answer tbody').addEventListener('mouseup', change_command);
+}
+function map_touchstart (e) {
+  obj.start_x = e.touches[0].clientX;
+  obj.start_y = e.touches[0].clientY;
+  get_xz();
+  document.querySelector('#answer tbody').addEventListener('touchmove', all_removeE);
+  document.querySelector('#answer tbody').addEventListener('touchend', change_command);
+}
+const map = document.querySelector('#answer');
+const config = { childList: true, subtree: true };
+const callback = function(e) {
+  //reset
+  document.querySelector('#answer tbody').removeEventListener('mousedown', map_mousedown);
+  document.querySelector('#answer tbody').removeEventListener('touchstart', map_touchstart);
+  //input
+  document.querySelector('#answer tbody').addEventListener('mousedown', map_mousedown);
+  document.querySelector('#answer tbody').addEventListener('touchstart', map_touchstart);
+};
+const observer = new MutationObserver(callback);
+observer.observe(map, config);
+//^^^observer cp end^^^
+/*command copy*/
+document.getElementById("copyButton").addEventListener("click", function() {
+  // コピーするテキストを取得
+  var codeToCopy = document.getElementById("codeToCopy").innerText;
+
+  // 新しいテキストエリアを作成して、コピーするテキストを設定
+  var tempTextArea = document.createElement("textarea");
+  tempTextArea.value = codeToCopy;
+
+  // テキストエリアをページに追加
+  document.body.appendChild(tempTextArea);
+
+  // テキストエリアの選択範囲を設定し、コピーを実行
+  tempTextArea.select();
+  document.execCommand("copy");
+
+  // テキストエリアを削除
+  document.body.removeChild(tempTextArea);
+  document.getElementById("copyButton").innerHTML = '<i class="fa-solid fa-check"></i>';
+  //alert("コピーしました！");
+});
 //全角を半角に変更
 /*https://www.searchlight8.com/jquery-javaascript-replace-charcode/*/
 $(function () {
@@ -224,7 +347,6 @@ $(function () {
     let str = val.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) {
       return String.fromCharCode(s.charCodeAt(0) - 0xfee0);
     });
-
     if (val.match(/[Ａ-Ｚａ-ｚ０-９]/g)) {
       $(e).val(str);
     }
