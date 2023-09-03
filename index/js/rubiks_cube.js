f659cc78851d54715c95300bef8ccea11d39d19d
Yusuke M <126371351+minecraft-app-yv@users.noreply.github.com>
2023-09-04 04:20:43 +0900

    Add files via upload

--- index/js/rubiks_cube.js
+++ index/js/rubiks_cube.js
@@ -16,12 +16,11 @@ if (width > height) {
   wSize = height;
 }
 // キューブが画面内に収まる最大のZ位置を計算
-let maxZ = cubeDiagonal * 1000 / (wSize - 60);
-if (maxZ <= 1000) {
-  maxZ = 1000;
+let maxZ = cubeDiagonal * 1000 / (wSize);
+if (maxZ <= 500) {
+  maxZ = 500;
 }
 camera.position.set(0, 0, maxZ);
-const cameraPosition = camera.position.clone();
 // ウィンドウのリサイズ時にキャンバスのサイズを調整する
 window.addEventListener("resize", () => {
   const newWidth = window.innerWidth;
@@ -53,8 +52,16 @@ window.addEventListener("resize", () => {
 });
 // キューブの作成
 const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+const textureUrls = [
+  '../art/img/blocks/Block_of_Diamond.jpg',
+  '../art/img/blocks/Block_of_Emerald.jpg',
+  '../art/img/blocks/Block_of_Gold.jpg',
+  '../art/img/blocks/Block_of_Amethyst.jpg',
+  '../art/img/blocks/Copper_Block.jpg',
+  '../art/img/blocks/Sea_Lantern.jpg',
+];
 const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff]; // 各面の色
-const materials = colors.map(color => new THREE.MeshBasicMaterial({
+let materials = colors.map(color => new THREE.MeshBasicMaterial({
   color
 }));
 const lineMaterial = new THREE.LineBasicMaterial({
@@ -74,6 +81,42 @@ for (let x = -1; x <= 1; x++) {
     }
   }
 }
+//change image
+// ルービックキューブの初期化関数
+function initializeRubiksCube() {
+  // 古いルービックキューブを削除
+  scene.children.length = 0;
+  // 新しいルービックキューブを生成
+  for (let x = -1; x <= 1; x++) {
+    for (let y = -1; y <= 1; y++) {
+      for (let z = -1; z <= 1; z++) {
+        const cube = new THREE.Mesh(cubeGeometry, materials);
+        cube.position.set(x * cubeSize, y * cubeSize, z * cubeSize);
+        scene.add(cube);
+        // 境界線の描画
+        const edges = new THREE.EdgesGeometry(cubeGeometry);
+        const lineSegments = new THREE.LineSegments(edges, lineMaterial);
+        lineSegments.position.set(0, 0, 0); // 境界線のずれなし
+        cube.add(lineSegments); // cube の子要素として追加する
+      }
+    }
+  }
+}
+$('#change_image').change((e) => {
+  if ($('#change_image').prop('checked')) {
+    materials = colors.map((color, index) => {
+      const textureLoader = new THREE.TextureLoader();
+      const texture = textureLoader.load(textureUrls[index]);
+      return new THREE.MeshBasicMaterial({ map: texture });
+    });
+  }
+  if (!$('#change_image').prop('checked')) {
+    materials = colors.map(color => new THREE.MeshBasicMaterial({
+      color
+    }));
+  }
+  initializeRubiksCube();
+});
 let controls = new OrbitControls(camera, canvasElement);
 // マウス座標からのレイキャスティング
 const raycaster = new THREE.Raycaster();
@@ -81,14 +124,157 @@ const mouse = new THREE.Vector2();
 let intersectedObject = null;
 let canvasWidth = canvasElement.clientWidth; // キャンバスの幅
 let canvasHeight = canvasElement.clientHeight; // キャンバスの高さ
+//shuffle
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomAxis() {
+  const axes = ['x', 'y', 'z'];
+  const randomIndex = getRandomInt(0, 2);
+  return axes[randomIndex];
+}
+function getRandomDirection() {
+  return Math.random() < 0.5 ? 1 : -1;
+}
+function shuffle() {
+  let shuffleCount = $("#shuffle-value").val();
+  for (let i = 0; i < shuffleCount; i++) {
+    const selectedCube = scene.children[getRandomInt(0, scene.children.length - 1)];
+    if (selectedCube instanceof THREE.Mesh) {
+      const cubeSpacing = cubeSize;
+      const randomAxes = getRandomAxis();
+      const angleChange = (Math.PI / 2) * getRandomDirection();
+      if (randomAxes[0] === 'x') {
+        let select_x_axis = [
+          {x: 0, y: -1, z: -1}, {x: 0, y: 0, z: -1}, {x: 0, y: 1, z: -1},
+          {x: 0, y: -1, z: 0}, {x: 0, y: 0, z: 0}, {x: 0, y: 1, z: 0},
+          {x: 0, y: -1, z: 1}, {x: 0, y: 0, z: 1}, {x: 0, y: 1, z: 1}
+        ];
+        select_x_axis.forEach(function(item, index) {
+          // 選択キューブの座標を計算
+          const cubeNextPosition = new THREE.Vector3(
+            Math.round(selectedCube.position.x) + cubeSpacing * item.x,
+            cubeSpacing * item.y,
+            cubeSpacing * item.z
+          );
+          // 選択キューブを検索
+          const cubeNext = scene.children.find(child => {
+            if (child instanceof THREE.Mesh) {
+              const position = child.position.clone();
+              position.x = Math.round(position.x); // x座標にMath.floorを適用
+              position.y = Math.round(position.y); // y座標にMath.floorを適用
+              position.z = Math.round(position.z); // z座標にMath.floorを適用
+              return cubeNextPosition.equals(position);
+            }
+          });
+          // 選択キューブをハイライト
+          if (cubeNext) {
+            select_x_axis[index] = cubeNext;
+          }
+        });
+        select_x_axis.forEach((cube, index) => {
+          const offsetY = cube.position.y / 100;
+          const offsetZ = cube.position.z / 100;
+          const calc = calculateDistanceAndAngleChange(0, 0, offsetY, offsetZ);
+          cube.position.y -= cubeSize * offsetY;
+          cube.position.z -= cubeSize * offsetZ;
+          const rotationAxis = new THREE.Vector3(1, 0, 0); // y軸回転
+          rotateObjectByAxisAngle(cube, rotationAxis, angleChange); // -angleChangeで逆方向に回転
+          cube.position.y += cubeSize * calc.dis * Math.cos(calc.ang + angleChange);
+          cube.position.z += cubeSize * calc.dis * Math.sin(calc.ang + angleChange);
+        });
+      }
+      if (randomAxes[0] === 'y') {
+        let select_y_axis = [
+          {x: -1, y: 0, z: -1}, {x: 0, y: 0, z: -1}, {x: 1, y: 0, z: -1},
+          {x: -1, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: 1, y: 0, z: 0},
+          {x: -1, y: 0, z: 1}, {x: 0, y: 0, z: 1}, {x: 1, y: 0, z: 1}
+        ];
+        select_y_axis.forEach(function(item, index) {
+          // 選択キューブの座標を計算
+          const cubeNextPosition = new THREE.Vector3(
+            cubeSpacing * item.x,
+            Math.round(selectedCube.position.y) + cubeSpacing * item.y,
+            cubeSpacing * item.z
+          );
+          // 選択キューブを検索
+          const cubeNext = scene.children.find(child => {
+            if (child instanceof THREE.Mesh) {
+              const position = child.position.clone();
+              position.x = Math.round(position.x); // x座標にMath.floorを適用
+              position.y = Math.round(position.y); // y座標にMath.floorを適用
+              position.z = Math.round(position.z); // z座標にMath.floorを適用
+              return cubeNextPosition.equals(position);
+            }
+          });
+          // 選択キューブをハイライト
+          if (cubeNext) {
+            select_y_axis[index] = cubeNext;
+          }
+        });
+        select_y_axis.forEach((cube, index) => {
+          const offsetX = cube.position.x / 100;
+          const offsetZ = cube.position.z / 100;
+          const calc = calculateDistanceAndAngleChange(0, 0, offsetX, offsetZ);
+          cube.position.x -= cubeSize * offsetX;
+          cube.position.z -= cubeSize * offsetZ;
+          const rotationAxis = new THREE.Vector3(0, 1, 0); // y軸回転
+          rotateObjectByAxisAngle(cube, rotationAxis, -angleChange); // -angleChangeで逆方向に回転
+          cube.position.x += cubeSize * calc.dis * Math.cos(calc.ang + angleChange);
+          cube.position.z += cubeSize * calc.dis * Math.sin(calc.ang + angleChange);
+        });
+      }
+      if (randomAxes[0] === 'z') {
+        let select_z_axis = [
+          {x: -1, y: -1, z: 0}, {x: 0, y: -1, z: 0}, {x: 1, y: -1, z: 0},
+          {x: -1, y: 0, z: 0}, {x: 0, y: 0, z: 0}, {x: 1, y: 0, z: 0},
+          {x: -1, y: 1, z: 0}, {x: 0, y: 1, z: 0}, {x: 1, y: 1, z: 0}
+        ];
+        select_z_axis.forEach(function(item, index) {
+          // 選択キューブの座標を計算
+          const cubeNextPosition = new THREE.Vector3(
+            cubeSpacing * item.x,
+            cubeSpacing * item.y,
+            Math.round(selectedCube.position.z) + cubeSpacing * item.z
+          );
+          // 選択キューブを検索
+          const cubeNext = scene.children.find(child => {
+            if (child instanceof THREE.Mesh) {
+              const position = child.position.clone();
+              position.x = Math.round(position.x); // x座標にMath.floorを適用
+              position.y = Math.round(position.y); // y座標にMath.floorを適用
+              position.z = Math.round(position.z); // z座標にMath.floorを適用
+              return cubeNextPosition.equals(position);
+            }
+          });
+          // 選択キューブをハイライト
+          if (cubeNext) {
+            select_z_axis[index] = cubeNext;
+          }
+        });
+        select_z_axis.forEach((cube, index) => {
+          const offsetX = cube.position.x / 100;
+          const offsetY = cube.position.y / 100;
+          const calc = calculateDistanceAndAngleChange(0, 0, offsetX, offsetY);
+          cube.position.x -= cubeSize * offsetX;
+          cube.position.y -= cubeSize * offsetY;
+          const rotationAxis = new THREE.Vector3(0, 0, 1); // y軸回転
+          rotateObjectByAxisAngle(cube, rotationAxis, angleChange); // -angleChangeで逆方向に回転
+          cube.position.x += cubeSize * calc.dis * Math.cos(calc.ang + angleChange);
+          cube.position.y += cubeSize * calc.dis * Math.sin(calc.ang + angleChange);
+        });
+      }
+    }
+  }
+}
+document.getElementById('shuffle-button').addEventListener('click', shuffle);
 // mousemove fun
 let obj = {
   start_x: '', start_y: '', end_x: '', end_y: '',
   targetCube: '', moveCube: '', select_cube: '', cubes: '', changeKey: 'x', vector: ''
 };
-// ボタン要素を取得
-const upButton = document.getElementById('up-button');
-const downButton = document.getElementById('down-button');
 function rotateObjectByAxisAngle(object, axis, angle) {
   const rotationMatrix = new THREE.Matrix4().makeRotationAxis(axis, angle);
   object.applyMatrix4(rotationMatrix);
@@ -280,9 +466,9 @@ function removeEvent(e) {
 function moveFunc(event) {
   let xRang = Math.abs(obj.start_x - obj.end_x);
   let yRang = Math.abs(obj.start_y - obj.end_y);
-  if (yRang >= Math.sqrt(3) * xRang) {
+  if (yRang >= 3 * xRang) {
     highlightAxis(obj.vector[0]);
-  } else if (xRang >= Math.sqrt(3) * yRang) {
+  } else if (xRang >= 3 * yRang) {
     highlightAxis(obj.vector[1]);
   } else {
     highlightAxis(obj.vector[2]);
@@ -604,3 +790,19 @@ function animate() {
   requestAnimationFrame(animate);
 }
 animate();
+//pop pop_text
+function changeDifficulty() {
+  let value = $('#shuffle-value').val();
+  let diff_text = $('<div>').attr('id', 'pop_text').text(value);
+  let p = $('#shuffle-value')[0].getBoundingClientRect();
+  let d = diff_text[0].getBoundingClientRect();
+  let scrollTop = window.scrollY;
+  let left = p.left + p.width + 10;
+  let top = p.top + scrollTop - (d.height);
+  diff_text.css({ 'position': 'absolute', 'left': left, 'top': top });
+  $('body').append(diff_text);
+  setTimeout(() => {
+    diff_text.remove();
+  }, 1000);
+}
+$("#shuffle-value").on("input", changeDifficulty);
